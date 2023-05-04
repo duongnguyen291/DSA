d7da927a5c0a35e466a1a4cd4373c1060cc31eef
duongnguyen291 <2901nguyendinhduong@gmail.com>
2023-05-05 01:19:38 +0700

    home5-2

--- labs/week4/ex5.cpp
+++ labs/week4/ex5.cpp
@@ -3,22 +3,14 @@
 using namespace std;
 int a[50],n,m;
 void sol(int p, int sum){
-    if(p == n && (m - sum) >= a[p-1]){
+    if(p == n){
         for(int i = 1; i < n;i++) cout << a[i] << " ";
         cout << m - sum << " "<<  endl;
         return;
     }
-    if(p == 1){
     for(int i = 1;i <= m - sum - (n - p); i++){
         a[p] = i;
         sol(p+1,sum+i);
-    }
-    }
-    else{
-        for(int i = a[p-1];i <= m - sum - (n - p); i++){
-        a[p] = i;
-        sol(p+1,sum+i);
-    }
     }
 }
 
--- labs/week4/ex7.cpp
+++ labs/week4/ex7.cpp
@@ -1,91 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define N 25
-#define M 25
 
 int n, k, r, c;
-int a[N][M];
-int check_row(int m){
-    //return 1 if not same row
-    //otherwise return 0;
-    int sum;
-    for(int i = 0;i < m; i++){
-        sum = 0;
-        for(int j = 0; j < m; j++ ){
-            sum += a[i][j];
-        }
-        if(sum > 1) return 0;
+int a[N];
+int cnt = 0;
+int check(int k, int j){
+    for(int i = 1; i <= n;i++){
+        if(a[i] == j || abs(a[i] - j) == abs(i - k)) return 0;
     }
     return 1;
 }
-int check_column(int m){
-    //return 1 if not same column
-    //otherwise return 0;
-    int sum;
-    for(int j = 0; j < m; j++){
-            sum = 0;
-        for(int i = 0; i < m; i++){
-            sum+= a[i][j];
-        }
-        if(sum > 1) return 0;
-    }
-    return 1;
-}
-int check_dia(int m){
-    //return 1 if not same dia
-    //otherwise return 0;
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < m; j++){
-            if(a[i][j] == 1){
-                //check the dia from left above to righ below
-                for(int l = i + 1,  k = j + 1; l < m && k < m; l++,k++){
-                    if(a[i][j] == a[l][k]) return 0;
-                }
-                //check the dia from righ above to left below
-                for(int l = i - 1,  k = j - 1; l >= 0 && k >= 0; l--, k--){
-                    if(a[i][j] == a[l][k]) return 0;
-                }               
-            }
+void Try(int k){
+    //k: column
+    for(int j = 1; j <= n; j++){
+        //kiểm tra xem đặt hậu ở cột k, hàng j có đụng không
+        if(check(k,j)){
+            //nếu không đụng, thì đặt lưu biến tại cột k, hàng j
+            a[k] = j;
+            //nếu k = n nghĩa là đã xét xong hết bàn cờ: giờ in ra hoặc count++
+            if(k == n) cnt++; 
+            //k chưa bằng n nghĩa là chưa xét xong , xét tiếp đến cột k + 1
+            else Try(k+1);
         }
     }
-    return 1;
-}
 
-// Kiểm tra xem quân hậu đặt ở hàng i và cột j có bị tấn công bởi
-// một quân hậu nào khác hay không
-int check(int i, int j) {
-    a[i][j] = 1;
-    if(check_column(n)&&check_row(n)&&check_dia(n)) return 1;    
-    else {
-        a[i][j] = 0;
-        return 0;
-    }
 }
-
-// Thuật toán quay lui để đặt quân hậu vào bàn cờ
-int solve(int i) {
-    if (i > n) {
-        return 1;
-    }
-    int count = 0;
-    for (int j = 1; j <= n; j++) {
-        if (!a[i][j] && check(i, j)){
-            a[i][j] = 1;
-            count += solve(i+1);
-            a[i][j] = 0;
-        }
-    }
-    return count;
-}
-
 
 int main() {
     cin >> n >> k;
     for (int i = 0; i < k; i++) {
         cin >> r >> c;
-        a[r][c] = 1;
+        if(a[r]!=0) {cout <<"da co con hau o tren cot nay" ; break;}
+        a[r] = c;
     }
-    cout << solve(1) << endl;
+    Try(1);
+    cout << cnt;
     return 0;
 }
