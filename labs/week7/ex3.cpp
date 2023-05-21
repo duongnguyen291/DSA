0de24a72a53da6035a4ffe7be1dabdf510193c88
duongnguyen291 <2901nguyendinhduong@gmail.com>
2023-05-21 18:58:52 +0700

    home-7

--- labs/week7/ex3.cpp
+++ labs/week7/ex3.cpp
@@ -12,18 +12,6 @@ node *makeNode(int v){
     new_node->next = NULL;
     return new_node;
 }
-node *InsertToLast(node *head, int X){ //pass by value
-    node *new_node = makeNode(X);
-    if(head == NULL) head = new_node;
-    else{
-        node *last = head;
-        while(last->next != NULL) last = last->next; //move to the last node
-        last->next = new_node;
-    }
-    return head;
-}
-//addfirst k
-
 node *findNode(node *head, int k){
     node *cur = head;
     while(cur!=NULL){
@@ -32,18 +20,39 @@ node *findNode(node *head, int k){
     }
     return cur;
 }
+node *InsertToLast(node *head, int X){ //pass by value
+    node *new_node = makeNode(X);
+    if(head == NULL) head = new_node;
+    else{
+        node *p = findNode(head,X);
+        if(p==NULL){
+        node *last = head;
+        while(last->next != NULL) last = last->next; //move to the last node
+        last->next = new_node;
+        }
+    }
+    return head;
+}
+//addfirst k
+
+
 node *InsertToHead(node *head, int X){
     if(head == NULL) return makeNode(X);
     node *new_node = makeNode(X);
     new_node -> next = head;
-    return new_node;
+    head = new_node;
+    return head;
 }
 node *InsertAfter(node *head, int u,int v){
     if(head == NULL) return NULL;
+    //check xem có v không
     node *p = findNode(head,v);
     if(p == NULL) return head;
+    //check xem đã tồn tại u chưa
+    node *q = findNode(head,u);
+    if(q != NULL) return head;
     node *new_node = makeNode(u);
-    new_node -> next = p->next;
+    new_node ->next = p->next;
     p->next = new_node;
     return head;
 }
@@ -51,11 +60,11 @@ node *InsertBefore(node *head, int u, int v){
     if(head == NULL) return NULL;
     if(head ->data == v ) return InsertToHead(head,u);
     node *prev = NULL, *p = head;
-    while(p!=NULL){ 
-        if(p->data = v) break;
+    while(p!=NULL && p->data != v){
         prev = p;
         p = p->next;
-    }   
+    }
+
     if(p == NULL) // tức là không tồn tại  p = v
         return head;
     node *new_node = makeNode(u);
@@ -65,25 +74,37 @@ node *InsertBefore(node *head, int u, int v){
 }
 node *removeNode(node *head, int k){
     if(head == NULL) return NULL;
-    if(head->data = k){ //vị trí k ở đầu
+    while(head->data == k){ //vị trí k ở đầu
         node *temp = head;
         head = head ->next;
         free(temp);
-        return head;
     }
-    else{   //vị trí k ở giữa hoặc cuối
         node *prev = NULL;
         node *p = head;
-        while(p!=NULL){
-            if(p->key == k) break;
-            prev = p; p = p->next;
-        }
-        if(p!=NULL){
-            prev->next = p->next;
-            free(p);
-        }
-        return head;
-    }
+        
+        //vị trí k ở giữa hoặc cuối
+        // while(p!=NULL){
+        //     if(p->data == k) break;
+        //     prev = p; p = p->next;
+        // }
+        // if(p!=NULL){
+        //     prev->next = p->next;
+        //     free(p);
+        // }
+            while (p != NULL) {
+                if (p->data == k) {
+                    node* temp = p;
+                    prev->next = p->next;
+                    p = p->next;
+                    free(temp);
+                } 
+                else {
+                    prev = p;
+                    p = p->next;
+                }
+            }
+    
+    return head;
 }
 node *revert(node *head){
     node *before, *cur = head,*after = cur->next;
@@ -94,18 +115,20 @@ node *revert(node *head){
         cur = after;
     }
     head = cur;
+    return head;
 }
 void printList(node *head){
     node *cur = new node();
     for(cur = head; cur !=NULL;cur= cur->next){
-        cout << cur->data;
-        
+        cout << cur->data <<" "; 
     }
+    cout << endl;
 }
 int main(){
     int n,v,k;
     node *head;
     head = NULL;
+    freopen("test3-2.txt","r",stdin);
     cin >> n;
     for(int i = 0;i < n;i++){
         cin >> v;
@@ -114,40 +137,40 @@ int main(){
     string s;
     while(true){
         cin >> s;
-        if(strcmp(s,"#") == 0) break;
-        if(strcmp(s,"addlast") == 0){
+        if(s == "#") 
+            break;
+        else if(s == "addlast"){
             cin >> k;
             if(findNode(head, k) == NULL)
                 head = InsertToLast(head, k);
         }
-        if(strcmp(s,"addfirst") == 0){
+        else if(s == "addfirst"){
             cin >> k;
             head = InsertToHead(head, k);
         }
-        if(strcmp(s,"addAfter") == 0){
+        else if(s == "addafter"){
             int d,m;
             cin >> d >> m ;
             if(findNode(head, d) != NULL)
-                head = InsertAfter(head,m,d);
+                head = InsertAfter(head,d,m);
         }
-        if(strcmp(s,"addBefore") == 0){
+        else if(s == "addbefore"){
             int d,m;
             cin >> d >> m ;
             if(findNode(head, d) != NULL)
-                head = InsertBefore(head,m,d);
+                head = InsertBefore(head,d,m);
         }
-        if(strcmp(s,"remove") == 0){
+        else if(s == "remove"){
             cin >> k;
             if(findNode(head,k) != NULL)
             head = removeNode(head, k);
         }
-        if(strcmp(s,"reverse") == 0){
-            cin >> k;
-            if(findNode(head,k) != NULL)
-            head = removeNode(head, k);
+        else if(s == "reverse"){
+            if(head != NULL)
+            head = revert(head);
         }
-
-
+        printList(head);
     }
+        // cout << 123;
     return 0;
 }
---
+++ labs/week7/test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    while(true){
+    if(s == "#") break;
+    else cout << 2;
+    
+    }
+    return 0;
+}
