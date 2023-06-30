4446586ba8ab8808700c26151f5f0616dbd5e727
duongnguyen291 <2901nguyendinhduong@gmail.com>
2023-06-30 20:25:29 +0700

    $1

--- labs/week13/ex2.cpp
+++ labs/week13/ex2.cpp
@@ -1,8 +1,8 @@
     //Problem: Dynamic Programming: Longest Common Subsequence
     #include <bits/stdc++.h>
     using namespace std;
-    #define size 100000
-    int memory[size][size] = {-1};
+    #define size 10000
+    int memory[size][size];
     int X[size], Y[size];
     int LCS(int i, int j){
         if(i == 0 || j == 0){
@@ -24,6 +24,11 @@
         }
         for(int i = 0; i < m;i++){
             cin >> Y[i];
+        }
+        for(int i = 0; i < n;i++){
+            for(int j = 0;j<m;j++){
+                memory[i][j] = -1;
+            }
         }
         int res = LCS(n,m);
         cout << res;
